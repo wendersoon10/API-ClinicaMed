09acda355dacf31811a3ae87a2b994be6b5304e8
unknown <wendersoon010@gmail.com>
2026-05-06 15:10:45 -0300

    Nova migration

--- src/main/java/med/clinica/api/medicos/DadosCadastroMedico.java
+++ src/main/java/med/clinica/api/medicos/DadosCadastroMedico.java
@@ -16,6 +16,9 @@ public record DadosCadastroMedico(
         String email,
 
         @NotBlank
+        String telefone,
+
+        @NotBlank
         @Pattern(regexp = "\\d{4,6}")
         String crm,
 
