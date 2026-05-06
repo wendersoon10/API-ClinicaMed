97ee5e2cdb95e31f9cb86816d7b9a6a8282b4b9e
unknown <wendersoon010@gmail.com>
2026-05-06 15:50:16 -0300

    Migration e validações pacientes

--- src/main/java/med/clinica/api/controller/ControllerPaciente.java
+++ src/main/java/med/clinica/api/controller/ControllerPaciente.java
@@ -1,6 +1,9 @@
 package med.clinica.api.controller;
 
+import jakarta.validation.Valid;
 import med.clinica.api.pacientes.DadosCadastroPaciente;
+import med.clinica.api.pacientes.Paciente;
+import med.clinica.api.pacientes.PacienteRepository;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/pacientes")
 public class ControllerPaciente {
 
+    private PacienteRepository repository;
+
+    public ControllerPaciente(PacienteRepository repository) {
+        this.repository = repository;
+    }
+
     @PostMapping
-    public void cadastro(@RequestBody DadosCadastroPaciente dados){
-        System.out.println(dados);
+    public void cadastro(@RequestBody @Valid DadosCadastroPaciente dados){
+        repository.save(new Paciente(dados));
     }
 }
---
+++ src/main/java/med/clinica/api/pacientes/PacienteRepository.java
@@ -0,0 +1,6 @@
+package med.clinica.api.pacientes;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PacienteRepository extends JpaRepository<Paciente, Long> {
+}
