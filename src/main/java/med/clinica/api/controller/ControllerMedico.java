c39fe23a4773fb63d001e9c0b8d44af8fa6e7134
unknown <wendersoon010@gmail.com>
2026-05-07 11:33:14 -0300

    Listagem médicos

--- src/main/java/med/clinica/api/controller/ControllerMedico.java
+++ src/main/java/med/clinica/api/controller/ControllerMedico.java
@@ -2,11 +2,13 @@ package med.clinica.api.controller;
 
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
-import med.clinica.api.endereco.Endereco;
 import med.clinica.api.medicos.DadosCadastroMedico;
+import med.clinica.api.medicos.DadosListagemMedico;
 import med.clinica.api.medicos.Medico;
 import med.clinica.api.medicos.MedicoRepository;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -23,5 +25,10 @@ public class ControllerMedico {
     @Transactional
     public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados){
         repository.save(new Medico(dados));
+    }
+
+    @GetMapping
+    public List<DadosListagemMedico> listar(){
+        return repository.findAll().stream().map(DadosListagemMedico::new).toList();
     }
 }
