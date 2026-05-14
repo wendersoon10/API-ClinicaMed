b3b70dd48006c9ecd95a71ea2ee877cb07a0098d
unknown <wendersoon010@gmail.com>
2026-05-14 10:41:22 -0300

    Adição do Spring Security, nova entidade e migration Usuario

---
+++ src/main/java/med/clinica/api/domain/usarios/Usuario.java
@@ -0,0 +1,21 @@
+package med.clinica.api.domain.usarios;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "usuarios")
+@Entity(name = "Usuario")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Usuario {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String login;
+    private String senha;
+}
